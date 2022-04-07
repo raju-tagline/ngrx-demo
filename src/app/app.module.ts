b02218467a37983c7687f6aa2ab3280d27401384
raju-tagline <raju.tagline@gmail.com>
2022-04-07 11:33:07 +0530

    Remove unwante code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,34 @@
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { PostList } from './Store/post.effect';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { StoreModule } from '@ngrx/store';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GraphQLModule } from './Graph Module/graphql.module';
+import { HttpClientModule } from '@angular/common/http';
+import { FirstComponent } from './first/first.component';
+import { postReducer } from './Store/post.reducer';
+import { EffectsModule } from '@ngrx/effects';
+import { counterReducer } from './State/state.reducer';
+import { CounterComponent } from './counter/counter.component';
+import { getDataList } from './State/state.effect';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, FirstComponent, CounterComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    EffectsModule.forRoot([getDataList]),
+    StoreModule.forRoot({ counter: counterReducer }),
+    StoreDevtoolsModule.instrument({
+      maxAge: 100,
+    }),
+    GraphQLModule,
+    HttpClientModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
